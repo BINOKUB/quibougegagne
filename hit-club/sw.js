5a7523758c088697e131c4c51a02359be36a78d4
BINOKUB <binokub@gmail.com>
2026-05-16 17:52:51 -0400

    Update sw.js

--- hit-club/sw.js
+++ hit-club/sw.js
@@ -1,5 +1,4 @@
-const CACHE_NAME = 'hit-club-v1';
-// On liste ici tout ce qui doit survivre sans internet
+const CACHE_NAME = 'hit-club-v2';
 const ASSETS = [
     './',
     './index.html',
@@ -11,8 +10,19 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+    self.skipWaiting();
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.map((key) => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            }));
+        })
     );
 });
 
